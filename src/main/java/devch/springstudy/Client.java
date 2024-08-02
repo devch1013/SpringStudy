9a938ca86145e4d810b821be8657ddcdf482ff7a
devch1013 <devch1013@gmail.com>
2024-08-02 18:24:26 +0900

    ObjectFactory 도입

--- src/main/java/devch/springstudy/Client.java
+++ src/main/java/devch/springstudy/Client.java
@@ -4,7 +4,8 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        PaymentService paymentService = new PaymentService();
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
