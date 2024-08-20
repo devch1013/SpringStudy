9d07166919530ed67ab71172dfdfb5969b0874a5
devch1013 <devch1013@gmail.com>
2024-08-20 14:13:52 +0900

    여러 경우의 수에 대한 테스트(메소드 분리)

--- src/test/java/devch/springstudy/payment/PaymentServiceTest.java
+++ src/test/java/devch/springstudy/payment/PaymentServiceTest.java
@@ -1,34 +1,34 @@
 package devch.springstudy.payment;
 
-import devch.springstudy.exrate.WebApiExRateProvider;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare가 잘 돌아가는지 검증")
-    void prepare() throws Exception {
+    void convertedAmount() throws Exception {
         // 준비
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(500)));
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+        // 원화환산 금액 유효시간
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws Exception {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
         // 실행
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보 가져오기
-        assertThat(payment.getExRate()).isEqualTo(valueOf(500));
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
         // 금액계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(valueOf(5_000));
-        // 원화환산 금액 유효시간
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
