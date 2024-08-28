9a942eeb5e7cda7043d35cdad8d9ba9c733525bd
devch1013 <devch1013@gmail.com>
2024-08-28 14:59:55 +0900

    Stub object 도입

--- src/test/java/devch/springstudy/payment/PaymentServiceSpringTest.java
+++ src/test/java/devch/springstudy/payment/PaymentServiceSpringTest.java
@@ -4,28 +4,36 @@ import devch.springstudy.ObjectFactory;
 import devch.springstudy.TestObjectFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
-
+    @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
     @Test
-    @DisplayName("prepare가 잘 돌아가는지 검증")
     void convertedAmount() throws Exception {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class); // object factory를 사용해서 bean factory를 구성해라!
-        // ObjectFactory에도 annotation 달아줘야함
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         // 실행
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보 가져오기
         assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
         // 금액계산
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
     }
 
 }
