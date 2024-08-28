c8ab8b309e74cc4fdc3a84add447efa053f5c65a
devch1013 <devch1013@gmail.com>
2024-08-28 15:51:30 +0900

    clock 사용한 테스트

--- src/test/java/devch/springstudy/payment/PaymentServiceSpringTest.java
+++ src/test/java/devch/springstudy/payment/PaymentServiceSpringTest.java
@@ -1,6 +1,7 @@
 package devch.springstudy.payment;
 
 import devch.springstudy.TestPaymentConfig;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,8 +18,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestPaymentConfig.class)
 class PaymentServiceSpringTest {
-    @Autowired PaymentService paymentService;
-    @Autowired ExRateProviderStub exRateProviderStub;
+    @Autowired
+    PaymentService paymentService;
+    @Autowired
+    Clock clock;
+    @Autowired
+    ExRateProviderStub exRateProviderStub;
+
     @Test
     void convertedAmount() throws Exception {
         // 실행
@@ -30,6 +38,15 @@ class PaymentServiceSpringTest {
         Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
         assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+    }
+
+    @Test
+    void validUntil() throws Exception {
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+
     }
 
 }
--- src/test/java/devch/springstudy/payment/PaymentServiceTest.java
+++ src/test/java/devch/springstudy/payment/PaymentServiceTest.java
@@ -1,29 +1,51 @@
 package devch.springstudy.payment;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.*;
 
 class PaymentServiceTest {
 
+    Clock clock;
+    @BeforeEach
+    void beforeEach() {
+        this.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+    }
+
     @Test
     @DisplayName("prepare가 잘 돌아가는지 검증")
     void convertedAmount() throws Exception {
         // 준비
-        testAmount(valueOf(500), valueOf(5_000));
-        testAmount(valueOf(1_000), valueOf(10_000));
-        testAmount(valueOf(3_000), valueOf(30_000));
-        // 원화환산 금액 유효시간
-//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        testAmount(valueOf(500), valueOf(5_000), this.clock);
+        testAmount(valueOf(1_000), valueOf(10_000), this.clock);
+        testAmount(valueOf(3_000), valueOf(30_000), this.clock);
     }
 
-    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws Exception {
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+    @Test
+    void validUntil() throws Exception {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(1_000)), this.clock);
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        LocalDateTime now = LocalDateTime.now(clock);
+
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) throws Exception {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate), clock);
         // 실행
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보 가져오기
